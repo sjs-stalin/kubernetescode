ec8dafde71975566834aaa05f0f41eceb6e922c4
sjs-stalin <84672923+sjs-stalin@users.noreply.github.com>
2023-04-13 13:11:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'EKS auto build from GITHUB code, TY!!!'
+    return 'One touch app build using devops on EKS TY!!! *****************Thanks**************'
